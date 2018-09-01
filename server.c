769980e545b0a79b8604d558bb93a456af904a5f
NamanJain8 <jnaman806@gmail.com>
2018-09-02 04:55:53 +0530

    Merged export.c functionality with server.c

--- server.c
+++ server.c
@@ -1,10 +1,12 @@
 #include <stdio.h>
 #include <netinet/in.h>
 #include <string.h>
+#include <stdlib.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
 #include <math.h>
 
+typedef char* string;
 
 int main()
 {
@@ -66,6 +68,73 @@ int main()
 
 		///////////////////// Parsed /////////////////////////////
 
+		//// Create HTML file ///////////////////
+
+		FILE *out_file = fopen("files/index.html", "w");
+		int t= trucks, d=dogs,c=cats, car=cars;
+
+		string a="",b="",n="",n1="";
+
+
+    if(t==1){
+        a = "<div class=\"column1\"><img src=\"truck1.jpg\" style=\"width:100%%\"></div>";
+    }
+    else if(t==2){
+        a = "<div class=\"column2\"><img src=\"truck1.jpg\" style=\"width:100%%\"></div><div class=\"column2\"><img src=\"truck2.jpg\" style=\"width:100%%\"></div>";
+    }
+    else if(t==3){
+        a = "<div class=\"column3\"><img src=\"truck1.jpg\" style=\"width:100%%\"></div><div class=\"column3\"><img src=\"truck2.jpg\" style=\"width:100%%\"></div><div class=\"column3\"><img src=\"truck3.jpg\" style=\"width:100%%\"></div>";
+    }
+    else if(t==4){
+        a = "<div class=\"column4\"><img src=\"truck1.jpg\" style=\"width:100%%\"></div><div class=\"column4\"><img src=\"truck2.jpg\" style=\"width:100%%\"></div><div class=\"column4\"><img src=\"truck3.jpg\" style=\"width:100%%\"></div><div class=\"column4\"><img src=\"truck4.jpg\" style=\"width:100%%\"></div>";
+    }
+
+
+    if(d==1){
+        b = "<div class=\"column1\"><img src=\"dog1.jpg\" style=\"width:100%%\"></div>";
+    }
+    else if(d==2){
+        b = "<div class=\"column2\"><img src=\"dog1.jpg\" style=\"width:100%%\"></div><div class=\"column2\"><img src=\"dog2.jpg\" style=\"width:100%%\"></div>";
+    }
+    else if(d==3){
+        b = "<div class=\"column3\"><img src=\"dog1.jpg\" style=\"width:100%%\"></div><div class=\"column3\"><img src=\"dog2.jpg\" style=\"width:100%%\"></div><div class=\"column3\"><img src=\"dog3.jpg\" style=\"width:100%%\"></div>";
+    }
+    else if(d==4){
+        b = "<div class=\"column4\"><img src=\"dog1.jpg\" style=\"width:100%%\"></div><div class=\"column4\"><img src=\"dog2.jpg\" style=\"width:100%%\"></div><div class=\"column4\"><img src=\"dog3.jpg\" style=\"width:100%%\"></div><div class=\"column4\"><img src=\"dog4.jpg\" style=\"width:100%%\"></div>";
+    }
+
+
+    if(c==1){
+        n = "<div class=\"column1\"><img src=\"cat1.jpg\" style=\"width:100%%\"></div>";
+    }
+    else if(c==2){
+        n = "<div class=\"column2\"><img src=\"cat1.jpg\" style=\"width:100%%\"></div><div class=\"column2\"><img src=\"cat2.jpg\" style=\"width:100%%\"></div>";
+    }
+    else if(c==3){
+        n = "<div class=\"column3\"><img src=\"cat1.jpg\" style=\"width:100%%\"></div><div class=\"column3\"><img src=\"cat2.jpg\" style=\"width:100%%\"></div><div class=\"column3\"><img src=\"cat3.jpg\" style=\"width:100%%\"></div>";
+    }
+    else if(c==4){
+        n = "<div class=\"column4\"><img src=\"cat1.jpg\" style=\"width:100%%\"></div><div class=\"column4\"><img src=\"cat2.jpg\" style=\"width:100%%\"></div><div class=\"column4\"><img src=\"cat3.jpg\" style=\"width:100%%\"></div><div class=\"column4\"><img src=\"cat4.jpg\" style=\"width:100%%\"></div>";
+    }
+
+    if(car==1){
+        n1 = "<div class=\"column1\"><img src=\"car1.jpg\" style=\"width:100%%\"></div>";
+    }
+    else if(car==2){
+        n1 = "<div class=\"column2\"><img src=\"car1.jpg\" style=\"width:100%%\"></div><div class=\"column2\"><img src=\"car2.jpg\" style=\"width:100%%\"></div>";
+    }
+    else if(car==3){
+        n1 = "<div class=\"column3\"><img src=\"car1.jpg\" style=\"width:100%%\"></div><div class=\"column3\"><img src=\"car2.jpg\" style=\"width:100%%\"></div><div class=\"column3\"><img src=\"car3.jpg\" style=\"width:100%%\"></div>";
+    }
+    else if(car==4){
+        n1 = "<div class=\"column4\"><img src=\"car1.jpg\" style=\"width:100%%\"></div><div class=\"column4\"><img src=\"car2.jpg\" style=\"width:100%%\"></div><div class=\"column4\"><img src=\"car3.jpg\" style=\"width:100%%\"></div><div class=\"column4\"><img src=\"car4.jpg\" style=\"width:100%%\"></div>";
+    }
+
+
+    fprintf(out_file, "    <html><head><style>* {    box-sizing: border-box;} img {display: block;    margin-left: auto;margin-right: auto;} .column3 {    float: left;    width: 33.33%%;    padding: 5px;} .column2 {    float: left;    width: 49%%;    padding: 5px;} .column4 {    float: left;    width: 25%%;    padding: 5px;} .column1 {    float: left;    width: 100%%;    padding: 5px;}/* Clearfix (clear floats) */.row::after {    content: "";    clear: both;    display: table;}</style><title>Fours Company Assignment-1</title>      </head>    <body bgcolor=\"5FFBB9\">    <div class=\"container-fluid\"><div class=\"row\">%s</div> <div class=\"row\">%s</div> <div class=\"row\">%s</div> <div class=\"row\">%s</div> </div>    </body></html>\n", a, b, n, n1);
+
+
+
 		char html[1024]="files/index.html";
 		FILE* fp = fopen(html, "rb");
 		fseek(fp, 0, 2);     //file pointer at the end of file 
@@ -95,8 +164,7 @@ int main()
 		{
 			perror("File");
 		}
-
-
+		
 		code = 555;
 		send(newSocket,(void *)&code,sizeof(int),0);
 	}
