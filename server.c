41f35b7566f684d6e2fd4e5963d44cea50e19f66
NamanJain8 <jnaman806@gmail.com>
2018-09-02 04:46:38 +0530

    Updated server file to parse client request and send random html file

--- server.c
+++ server.c
@@ -1,69 +1,104 @@
-/* credit @Daniel Scocco */
-
-/****************** SERVER CODE ****************/
-
 #include <stdio.h>
 #include <netinet/in.h>
 #include <string.h>
 #include <sys/socket.h>
 #include <arpa/inet.h>
+#include <math.h>
 
 
-int main(){
-  int welcomeSocket, newSocket;
-  char buffer[1024];
-  struct sockaddr_in serverAddr;
-  struct sockaddr_storage serverStorage;
-  socklen_t addr_size;
-
-  /*---- Create the socket. The three arguments are: ----*/
-  /* 1) Internet domain 2) Stream socket 3) Default protocol (TCP in this case) */
-  welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
-
-  /*---- Configure settings of the server address struct ----*/
-  /* Address family = Internet */
-  serverAddr.sin_family = AF_INET;
-  /* Set port number, using htons function to use proper byte order */
-  serverAddr.sin_port = htons(5432);
-  /* Set IP address to localhost */
-  serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
-  /* Set all bits of the padding field to 0 */
-  memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
-
-  /*---- Bind the address struct to the socket ----*/
-  bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
-
-  /*---- Listen on the socket, with 5 max connection requests queued ----*/
- while(1)
+int main()
 {
-  if(listen(welcomeSocket,5)==0)
-    printf("I'm listening\n");
-  else
-    printf("Error\n");
+	int welcomeSocket, newSocket;
+	char input[1024];
+	struct sockaddr_in serverAddr;
+	struct sockaddr_storage serverStorage;
+	socklen_t addr_size;
 
-  /*---- Accept call creates a new socket for the incoming connection ----*/
-  addr_size = sizeof serverStorage;
-  newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+	welcomeSocket = socket(PF_INET, SOCK_STREAM, 0);
 
-  /*---- Send message to the socket of the incoming connection ----*/
+	serverAddr.sin_family = AF_INET;
+	serverAddr.sin_port = htons(5432);
+	serverAddr.sin_addr.s_addr = inet_addr("127.0.0.1");
+	memset(serverAddr.sin_zero, '\0', sizeof serverAddr.sin_zero);
 
-  //strcpy(buffer,"Hello World\n")
-  recv(newSocket, buffer, 1024, 0);
-	printf("Request received for %s\n",buffer);
-	FILE* fp = fopen(buffer, "rb");
-        int ini;
-        if(fp != NULL){
-            while(fread(&ini, sizeof(int), 1, fp)) {
-		send(newSocket,(void *)&ini,sizeof(int),0);  	               
-            }
-	ini = 0;
-	send(newSocket,(void *)&ini,sizeof(int),0); 
-		printf("Done Sending\n");
-            fclose(fp);
-        } else {
-            perror("File");
-        }	
- 
-}
-  return 0;
+	bind(welcomeSocket, (struct sockaddr *) &serverAddr, sizeof(serverAddr));
+
+	while(1)
+	{
+		if(listen(welcomeSocket,5)==0)
+		  printf("I'm listening\n");
+		else
+		  printf("Error\n");
+
+		addr_size = sizeof serverStorage;
+		newSocket = accept(welcomeSocket, (struct sockaddr *) &serverStorage, &addr_size);
+
+	
+		recv(newSocket, input, 1024, 0);
+		printf("Request received for %s\n",input);
+
+		///////////// PARSING THE REQUEST ////////////
+		char *token = strtok(input, " ");
+		int cars=0,dogs=0,cats=0,trucks=0,i=0;
+
+		while(token) 
+		{
+		    if(token[0]>='a' && token[0]<='z')
+		    {
+		      if(token[0]=='c' && token[1]=='a' && token[2]=='r')
+		        cars = i;
+		      else if(token[0]=='t')
+		        trucks = i;
+		      else if(token[0]=='c' && token[1]=='a' && token[2]=='t')
+		        cats = i;
+		      else if(token[0]=='d')
+		        dogs = i;
+		    }
+		    else
+		    {
+		      i = token[0]-'0';
+		    }
+
+		    token = strtok(NULL, " ");
+		}
+
+		printf("%d cars , %d cats , %d trucks , %d dogs\n",cars,cats,trucks,dogs);
+
+		///////////////////// Parsed /////////////////////////////
+
+		char html[1024]="files/index.html";
+		FILE* fp = fopen(html, "rb");
+		fseek(fp, 0, 2);     //file pointer at the end of file 
+		int sz = ftell(fp);
+		int size,code,ini,flag=0;; 
+		fclose(fp); 
+
+		size = sz/4;
+		size += ( sz%4 == 0 ) ? 0:1;
+		code = 999;
+		
+		fp = fopen(html, "rb");
+		send(newSocket,(void *)&code,sizeof(int),0);
+		send(newSocket,(void *)&size,sizeof(int),0); 
+		printf("Start HTML send\n");
+		if(fp != NULL)
+		{
+		  while(fread(&ini, sizeof(int), 1, fp)) 
+		  {
+		    send(newSocket,(void *)&ini,sizeof(int),0);
+		    flag++;  	               
+		  }
+		  printf("HTML Sent \n \n");
+		  fclose(fp);
+		} 
+		else 
+		{
+			perror("File");
+		}
+
+
+		code = 555;
+		send(newSocket,(void *)&code,sizeof(int),0);
+	}
+	return 0;
 }
